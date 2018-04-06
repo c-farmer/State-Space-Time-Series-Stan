cb5ae8de0539778507791cb1423dad21fab71d83
c-farmer <38137796+c-farmer@users.noreply.github.com>
2018-04-06 12:37:40 -0600

    updated for Date format

--- Ch1/Ch1.R
+++ Ch1/Ch1.R
@@ -1,25 +1,26 @@
 library(tidyverse)
+library(lubridate)
 
 d <- read_csv("./data/UKdriversKSI.txt") %>% 
-  mutate(t = 1:nrow(d),
-         lKSI = log(KSI))
+  mutate(lKSI = log(KSI),
+         Date = ymd("1969-01-31") %m+% months(0:191))
 
 d %>%
-  ggplot(aes(t,lKSI)) + 
+  ggplot(aes(Date,lKSI)) + 
   geom_point() +
   geom_smooth(method = 'lm', se = FALSE) +
   labs(title = "Fig 1.1")
 
 d %>% 
-  ggplot(aes(t,lKSI)) + 
+  ggplot(aes(Date,lKSI)) + 
   geom_line() +
   labs(title = 'Fig 1.2')
 
-m1 <- lm(lKSI ~ t, data =d)
+m1 <- lm(lKSI ~ Date, data =d)
 
-tibble(t = d$t,
-       residuals = residuals(m1)) %>%
-  ggplot(aes(t, residuals)) +
+d %>% 
+  mutate(residuals = residuals(m1)) %>%
+  ggplot(aes(Date, residuals)) +
   geom_line() +
   labs(title = 'Fig 1.3')
 
